237996608ebc496b4ac9d7c1d392ab94f264ca30
Josh Blancett <joshblancett@gmail.com>
2014-06-18 15:54:25 -0400

    convert enabled attributes to booleans

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,10 +2,10 @@
 default['tsuru']['package']['repo_url'] = 'http://ppa.launchpad.net/tsuru/ppa/ubuntu'
 default['tsuru']['package']['repo_key'] = '383F073D'
 
-default['tsuru']['api_enabled'] = 'no'
-default['tsuru']['api_admin_enabled'] = 'no'
-default['tsuru']['collector_enabled'] = 'no'
-default['tsuru']['agent_enabled'] = 'no'
+default['tsuru']['api_enabled'] = false
+default['tsuru']['api_admin_enabled'] = false
+default['tsuru']['collector_enabled'] = false
+default['tsuru']['agent_enabled'] = false
 
 default['tsuru']['agent']['user'] = 'ubuntu'
 default['tsuru']['agent']['listen'] = '0.0.0.0:4545'
