4985ff76e779730b1c4af67923a77c68959a0f06
Josh Blancett <joshblancett@gmail.com>
2014-06-24 20:57:16 -0400

    gandalf-server recipe

---
+++ attributes/gandalf-server.rb
@@ -0,0 +1,7 @@
+
+default['gandalf-server']['bin-path'] = '/usr/bin/gandalf-ssh'
+default['gandalf-server']['git']['bare']['location'] = '/var/lib/gandalf/repositories'
+default['gandalf-server']['git']['bare']['template'] = '/home/git/bare-template'
+default['gandalf-server']['authorized-keys-path'] = '/home/git/.ssh/authorized_keys'
+default['gandalf-server']['host'] = 'localhost'
+default['gandalf-server']['bind'] = 'localhost:8000'
---
+++ recipes/gandalf-server.rb
@@ -0,0 +1,43 @@
+if node['platform'] == 'ubuntu'
+
+  include_recipe 'tsuru::repo'
+
+  package 'gandalf-server' do
+    action :upgrade
+  end
+
+  directory '/home/git/bare-template/hooks' do
+    action :create
+    owner 'git'
+    group 'git'
+    mode 0755
+    recursive true
+  end
+
+  cookbook_file '/home/git/bare-template/hooks/post-receive' do
+    action :create
+    owner 'git'
+    group 'git'
+    mode 0755
+    source 'post-receive'
+  end
+
+  template '/etc/gandalf.conf' do
+    action :create
+    owner 'root'
+    group 'root'
+    mode 0644
+    source 'gandalf.conf.erb'
+    variables gandalf: node['gandalf-server']
+    notifies :restart, 'service[gandalf-server]'
+  end
+
+  service 'gandalf-server' do
+    action [:enable, :start]
+  end
+
+  service 'git-daemon' do
+    action [:enable, :start]
+  end
+
+end
