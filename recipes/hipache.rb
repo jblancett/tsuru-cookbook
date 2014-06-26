83d40b7a66454457fd7b3169888382688aab4c78
Josh Blancett <joshblancett@gmail.com>
2014-06-26 17:15:20 -0400

    basic hipache recipe

---
+++ recipes/hipache.rb
@@ -0,0 +1,14 @@
+if node['platform'] == 'ubuntu'
+
+  include_recipe 'tsuru::repo'
+
+  package 'node-hipache' do
+    action :upgrade
+  end
+
+  service 'hipache' do
+    action [:enable, :start]
+    provider Chef::Provider::Service::Upstart if Chef::VersionConstraint.new('>= 13.10').include?(node['platform_version'])
+  end
+
+end
