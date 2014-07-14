a838f9b12e0b030e3f7d61e5355547af1d280f4a
Josh Blancett <joshblancett@gmail.com>
2014-07-13 21:03:54 -0400

    mongodb recipe

---
+++ attributes/mongodb.rb
@@ -0,0 +1,2 @@
+default['mongodb']['config']['port'] = 27017
+default['mongodb']['config']['bind_ip'] = '0.0.0.0'
--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,4 @@ description      'Installs/Configures tsuru'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.3.1'
 depends           'apt'
+depends           'mongodb'
