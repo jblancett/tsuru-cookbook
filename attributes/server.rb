83235bea962b0aa85aba49580184467099e797a6
Josh Blancett <joshblancett@gmail.com>
2014-07-13 21:20:29 -0400

    fixing up attribute detection

--- attributes/server.rb
+++ attributes/server.rb
@@ -8,7 +8,7 @@ default['tsuru']['server']['tls-key-file'] = nil # path to key
 default['tsuru']['server']['host'] = "http#{'s' if node['tsuru']['server']['use-tls']}://#{node.name}:#{node['tsuru']['server']['listen_port']}"
 default['tsuru']['server']['debug'] = false
 
-default['tsuru']['server']['database']['url'] = "127.0.0.1:#{node['mongodb']['config']['port'] rescue '27017'}"
+default['tsuru']['server']['database']['url'] = "127.0.0.1:#{node['mongodb']['config']['port'] || '27017' rescue '27017'}"
 default['tsuru']['server']['database']['name'] = 'tsuru'
 
 default['tsuru']['server']['git']['unit-repo'] = '/home/application/current'
@@ -33,10 +33,11 @@ default['tsuru']['server']['auth']['oauth']['info-url'] = nil
 default['tsuru']['server']['auth']['oauth']['collection'] = 'oauth_tokens'
 default['tsuru']['server']['auth']['oauth']['callback-port'] = nil
 
+redis_port = node['redis']['port'] || '6379' rescue '6379'
 default['tsuru']['server']['queue'] = 'redis' # beanstalkd is deprecated
 default['tsuru']['server']['queue-server'] = 'localhost:11300' # only used for beanstalkd
 default['tsuru']['server']['redis-queue']['host'] = '127.0.0.1'
-default['tsuru']['server']['redis-queue']['port'] = node['redis']['port'] || '6379'
+default['tsuru']['server']['redis-queue']['port'] = redis_port
 default['tsuru']['server']['redis-queue']['password'] = '' # blank for no password
 default['tsuru']['server']['redis-queue']['db'] = 3
 
@@ -47,7 +48,7 @@ default['tsuru']['server']['docker']['router'] = 'hipache'
 default['tsuru']['server']['docker']['deploy-cmd'] = '/var/lib/tsuru/deploy'
 default['tsuru']['server']['docker']['ssh-agent-port'] = node['tsuru']['agent']['listen'][/\d+$/] || '4545'
 default['tsuru']['server']['docker']['segregate'] = false
-default['tsuru']['server']['docker']['scheduler']['redis-server'] = "127.0.0.1:#{node['redis']['port']}"
+default['tsuru']['server']['docker']['scheduler']['redis-server'] = "127.0.0.1:#{redis_port}"
 default['tsuru']['server']['docker']['scheduler']['redis-prefix'] = 'docker-cluster'
 default['tsuru']['server']['docker']['run-cmd']['bin'] = '/var/lib/tsuru/start'
 default['tsuru']['server']['docker']['run-cmd']['port'] = '8888'
