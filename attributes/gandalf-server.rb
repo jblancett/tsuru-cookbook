235746960f6e351a0034d73065e21ddb51a2e09d
Josh Blancett <joshblancett@gmail.com>
2014-07-14 12:55:07 -0400

    gandalf-server bind to all interfaces by default

--- attributes/gandalf-server.rb
+++ attributes/gandalf-server.rb
@@ -4,5 +4,5 @@ default['gandalf-server']['git']['bare']['location'] = '/var/lib/gandalf/reposit
 default['gandalf-server']['git']['bare']['template'] = '/home/git/bare-template'
 default['gandalf-server']['authorized-keys-path'] = '/home/git/.ssh/authorized_keys'
 default['gandalf-server']['host'] = node.fqdn
-default['gandalf-server']['bind'] = 'localhost:8000'
+default['gandalf-server']['bind'] = '0.0.0.0:8000'
 default['gandalf-server']['token'] = nil
