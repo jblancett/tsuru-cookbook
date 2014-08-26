62590c311216466cb16c1dc1d302afafd953f5f3
Josh Blancett <joshblancett@gmail.com>
2014-08-26 16:26:15 -0400

    install mongodb from official ppa

--- recipes/mongodb.rb
+++ recipes/mongodb.rb
@@ -1,1 +1,20 @@
-include_recipe('mongodb')
+apt_repository 'mongodb' do
+  action :nothing
+  uri 'http://downloads-distro.mongodb.org/repo/ubuntu-upstart'
+  distribution 'dist'
+  components ['10gen']
+  keyserver 'keyserver.ubuntu.com'
+  key '7F0CEB10'
+  only_if { node['platform'] == 'ubuntu' }
+end.run_action(:add)
+
+package 'mongodb-org' do
+  action :upgrade
+  options '-o Dpkg::Options::="--force-confold"'
+  notifies :restart, 'service[mongod]'
+end
+
+service 'mongod' do
+	action :nothing
+	provider Chef::Provider::Service::Upstart
+end
