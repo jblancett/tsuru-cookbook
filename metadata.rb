f42c8733bf4126c11eedef60e7635757e79d595d
Josh Blancett <joshblancett@gmail.com>
2014-08-26 17:07:29 -0400

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'josh@firma8.com'
 license          'All rights reserved'
 description      'Installs/Configures tsuru'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.4'
+version          '0.5.0'
 depends           'apt'
 depends           'docker'
 depends           'aufs'
