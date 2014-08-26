d8b71475c66542a626045f1c83050f71fa65bebf
Josh Blancett <joshblancett@gmail.com>
2014-08-26 16:06:30 -0400

    removing other services

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,22 +1,15 @@
 
-include_recipe('tsuru::agent')
+include_recipe('tsuru::repo')
 
-service 'tsuru-server-api' do
-  action :nothing
-  supports :restart => true, :status => true
-  provider Chef::Provider::Service::Upstart
+package 'tsuru-server' do
+  action :upgrade
+  options '-o Dpkg::Options::="--force-confold"'
 end
 
 execute "sed -i 's/^TSR_API_ENABLED=.*/TSR_API_ENABLED=yes/' /etc/default/tsuru-server" do
   action :run
   notifies :start, 'service[tsuru-server-api]'
-  not_if 'cat /etc/default/tsuru-server | grep TSR_API_ENABLED=yes'
-end
-
-execute "sed -i 's/^TSR_API_ADMIN_ENABLED=.*/TSR_API_ADMIN_ENABLED=yes/' /etc/default/tsuru-server" do
-  action :run
-  notifies :start, 'service[tsuru-server-api]'
-  not_if 'cat /etc/default/tsuru-server | grep TSR_API_ADMIN_ENABLED=yes'
+  not_if 'grep ^TSR_API_ENABLED=yes /etc/default/tsuru-server'
 end
 
 template '/etc/tsuru/tsuru.conf' do
