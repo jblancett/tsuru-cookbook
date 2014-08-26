007f52cd3b05a6a9e8cb72a249e39bacdf20600d
Josh Blancett <joshblancett@gmail.com>
2014-08-26 16:11:46 -0400

    install redis-server from chris-lea ppa

--- recipes/redis.rb
+++ recipes/redis.rb
@@ -1,1 +1,13 @@
-include_recipe('redis')
+apt_repository 'redis-server' do
+  action :nothing
+  uri          'http://ppa.launchpad.net/chris-lea/redis-server/ubuntu'
+  distribution node['lsb']['codename']
+  components   ['main']
+  keyserver    'keyserver.ubuntu.com'
+  key          'C7917B12'
+end.run_action(:add)
+
+package 'redis-server' do
+  action :upgrade
+  options '-o Dpkg::Options::="--force-confold"'
+end
