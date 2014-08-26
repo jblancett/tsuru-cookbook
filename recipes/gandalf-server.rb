670e8c74412fb7d4a1980a4f7c3b5eeefaeb7dad
Josh Blancett <joshblancett@gmail.com>
2014-08-26 17:04:13 -0400

    cleaned up and improved gandalf-server recipe

--- recipes/gandalf-server.rb
+++ recipes/gandalf-server.rb
@@ -1,54 +1,50 @@
-if node['platform'] == 'ubuntu'
+include_recipe 'tsuru::repo'
 
-  include_recipe 'tsuru::repo'
+['gandalf-server','archive-server'].each do |pkg|
+  package pkg do
+    action :nothing
+    options '-o Dpkg::Options::="--force-confold"'
+  end.run_action(:upgrade)
+end
 
-  ['tsuru-server', 'gandalf-server','archive-server'].each do |pkg|
-    package pkg do
-      action :nothing
-      options '-o Dpkg::Options::="--force-confold"'
-    end.run_action(:upgrade)
+directory '/home/git/bare-template/hooks' do
+  action :create
+  owner 'git'
+  group 'git'
+  mode 0755
+  recursive true
+end
+
+cookbook_file '/home/git/bare-template/hooks/pre-receive' do
+  action :create
+  owner 'git'
+  group 'git'
+  mode 0755
+  source 'pre-receive'
+end
+
+template '/etc/gandalf.conf' do
+  action :create
+  owner 'root'
+  group 'root'
+  mode 0644
+  source 'gandalf.conf.erb'
+  variables gandalf: node['gandalf-server']
+  notifies :restart, 'service[gandalf-server]'
+end
+
+template '/home/git/.bash_profile' do
+  action :create_if_missing
+  owner 'root'
+  group 'root'
+  mode 0644
+  source 'bash_profile.erb'
+  variables tsuru_token: node['gandalf-server']['token'] || `which tsr && tsr token`
+end
+
+['gandalf-server','archive-server'].each do |svc|
+  service svc do
+    action [:enable, :start]
+    provider Chef::Provider::Service::Upstart if Chef::VersionConstraint.new('>= 13.10').include?(node['platform_version'])
   end
-
-  directory '/home/git/bare-template/hooks' do
-    action :create
-    owner 'git'
-    group 'git'
-    mode 0755
-    recursive true
-  end
-
-  cookbook_file '/home/git/bare-template/hooks/pre-receive' do
-    action :create
-    owner 'git'
-    group 'git'
-    mode 0755
-    source 'pre-receive'
-  end
-
-  template '/etc/gandalf.conf' do
-    action :create
-    owner 'root'
-    group 'root'
-    mode 0644
-    source 'gandalf.conf.erb'
-    variables gandalf: node['gandalf-server']
-    notifies :restart, 'service[gandalf-server]'
-  end
-
-  template '/home/git/.bash_profile' do
-    action :create_if_missing
-    owner 'root'
-    group 'root'
-    mode 0644
-    source 'bash_profile.erb'
-    variables tsuru_token: node['gandalf-server']['token'] || `tsr token`
-  end
-
-  ['gandalf-server','git-daemon','archive-server'].each do |svc|
-    service svc do
-      action [:enable, :start]
-      provider Chef::Provider::Service::Upstart if Chef::VersionConstraint.new('>= 13.10').include?(node['platform_version'])
-    end
-  end
-
 end
