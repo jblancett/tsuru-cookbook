44713bb89d80e13c40f955927710ba2397e35c21
Josh Blancett <joshblancett@gmail.com>
2014-08-26 17:07:16 -0400

    clean up dependencies

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,6 @@ description      'Installs/Configures tsuru'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.4.4'
 depends           'apt'
-depends           'mongodb'
-depends           'redis'
 depends           'docker'
 depends           'aufs'
 depends           'lxc'
